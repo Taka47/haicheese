92155d85ae52b1ff71c1cda3b57311e18e4becbf
Taka47 <piano47ohs@yahoo.co.jp>
2018-10-26 17:58:04 +0900

    登録したデータから、ログインが可能に。

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,14 +12,14 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="mailaddress" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="mailaddress" type="mailaddress" class="form-control{{ $errors->has('mailaddress') ? ' is-invalid' : '' }}" name="mailaddress" value="{{ old('mailaddress') }}" required autofocus>
 
-                                @if ($errors->has('email'))
+                                @if ($errors->has('mailaddress'))
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        <strong>{{ $errors->first('mailaddress') }}</strong>
                                     </span>
                                 @endif
                             </div>
